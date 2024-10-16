f4549c21a306d39051af340405330b4fbca1d3ad
gzj <1131600386@qq.com>
2024-10-17 00:02:23 +0800

    1

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -373,6 +373,10 @@ export const createGraph = (
               }
               visited.get(key2)?.add(key1);
               
+              if (!visited.has(key1)) {
+                visited.set(key1, new Set());
+              }
+              visited.get(key1)?.add(key2);
             }
           }
         }
