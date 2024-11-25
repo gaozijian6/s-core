17fe279de2e4b0d82dbdcb31d0a403975340792a
unknown <zijiangao2@creditease.cn>
2024-11-25 10:36:51 +0800

    1

--- src/views/sudoku.tsx
+++ src/views/sudoku.tsx
@@ -115,7 +115,7 @@ const Sudoku: React.FC = () => {
       }))
     );
 
-    newBoard = deepCopyBoard(mockBoard);
+    // newBoard = deepCopyBoard(mockBoard);
 
 
     updateBoard(newBoard, "生成新棋盘");
